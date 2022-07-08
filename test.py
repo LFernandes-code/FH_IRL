50588f06d761bfb7f0925aa54bf7df109f40be02
Luis Fernandes <luis.martins.fernandes@tecnico.ulisboa.pt>
2022-07-08 10:54:31 +0100

    Sword action

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     env = gym.make("FlowerHunter-v0", map_name = "Level1")
     #env.reset()
     #env.step(3)
-    a = []
-    if 2 not in a:
-        a.append(2)
+    a = [1,2,3]
+    a.remove(2)
+    a.remove(4)
+    print(a)
+    
