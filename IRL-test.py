608e0156cd6f90b79ced9228b9d548181b12addb
SirPanda <luis.martins.fernandes@tecnico.ulisboa.pt>
2022-07-14 10:28:58 +0100

    Setup IRL (GAIL)

--- IRL-test.py
+++ IRL-test.py
@@ -1,15 +1,50 @@
 import gym
 import seals
 import gym_game
-from imitation.data.types import TrajectoryWithRew
-import numpy as np
+import time
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from Trace_generator import *
+from stable_baselines3 import PPO
+from imitation.algorithms.adversarial.gail import GAIL
+from imitation.rewards.reward_nets import BasicRewardNet
+from imitation.util.networks import RunningNorm
+from stable_baselines3.common.evaluation import evaluate_policy
 
 
 env = gym.make("FlowerHunter-v0", map_name = "Level1")
-
+venv = DummyVecEnv([lambda: gym.make("FlowerHunter-v0", map_name = "Level1")] * 8)
 #generate trajectories
-traj = []
-tr = TrajectoryWithRew([0,0],[1],None,terminal=True,rews=np.array([1.0]))
-traj.append(tr)
-print(traj)
-print(type(traj))
+rollouts = generate_trajectories("5_____10", "Level1_clusters", env)
+
+
+
+learner = PPO(
+    env=venv,
+    policy=MlpPolicy,
+    batch_size=64,
+    ent_coef=0.0,
+    learning_rate=0.0003,
+    n_epochs=10,
+)
+reward_net = BasicRewardNet(
+    venv.observation_space, venv.action_space, normalize_input_layer=RunningNorm
+)
+gail_trainer = GAIL(
+    demonstrations=rollouts,
+    demo_batch_size=1024,
+    gen_replay_buffer_capacity=2048,
+    n_disc_updates_per_round=4,
+    venv=venv,
+    gen_algo=learner,
+    reward_net=reward_net,
+)
+start_time = time.time()
+learner_rewards_before_training, _ = evaluate_policy(
+    learner, venv, 100, return_episode_rewards=True
+)
+gail_trainer.train(20000)  # Note: set to 300000 for better results
+learner_rewards_after_training, _ = evaluate_policy(
+    learner, venv, 100, return_episode_rewards=True
+)
+print("--- %s seconds ---" % (time.time() - start_time))
