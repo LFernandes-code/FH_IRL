24ee90772fb74645e119f07c24bde36f7865109c
SirPanda <luis.martins.fernandes@tecnico.ulisboa.pt>
2022-07-19 16:27:33 +0100

    Updates

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
+from shutil import register_unpack_format
 import sys
-import numpy as np
-import math
-import random
+import pickle
 
 import gym
 import gym_game
 from stable_baselines3.common import base_class
 
-class IRL_agent():
+class IRL_Agent():
     #base_class.BasePolicy
     def __init__(self, irl_alg, cluster):
         self.policy = self.load_policy(irl_alg, cluster)
-        pass
 
     def load_policy(self, irl_alg, cluster):
-        pass
+        irl_cluster_file = "IRL/" + irl_alg + "_" + cluster + ".pl"
+
+        with open(irl_cluster_file, 'rb') as f:
+            policy =  pickle.load(f)
+            return policy
 
     def select_action(self, obs):
         return self.policy.predict(obs)
 
 if __name__ == "__main__":
     env = gym.make("FlowerHunter-v0", map_name = "Level1")
-    env.reset()
+    agent = IRL_Agent("GAIL", "0")
+    obs = env.reset()
     done = False
+    act = agent.select_action(obs)
     while not done:
-        _, _, done,_ = env.step(5)
+        obs, _, done,_ = env.step(act)
+        act = agent.select_action(obs)
         env.render()
